7a2373f2071c60d0153a838aa3ab6b94fc3d5d50
Nima <nima.abed.ch@gmail.com>
2025-11-10 18:38:03 +0330

    step 4 and step 5 layouts added to signup page

--- src/signup.js
+++ src/signup.js
@@ -1,1 +1,16 @@
+// let step = 1;
 
+// let stepList = document.querySelectorAll(".step");
+
+// console.log(stepList);
+
+// stepList.forEach((state, index) => {
+//   // if (step === index) {
+//     // state.classList.add('block');
+//   state.classList.toggle('!block', step === index);
+//   // }
+//   // }else {
+//   //   state.classList.add('hidden');
+//   //   state.classList.remove('block');
+//   // }
+// })
